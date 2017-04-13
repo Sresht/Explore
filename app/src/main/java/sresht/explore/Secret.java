68c96eb7f6fe108a92cfe399c772c4f77be50215
Sresht Rengesh <sresht@gmail.com>
2017-04-13 00:29:13 -0700

    Commit dummy class to hold Secret keys.

---
+++ app/src/main/java/sresht/explore/Secret.java
@@ -0,0 +1,11 @@
+package sresht.explore;
+
+/**
+ * Holds Secret keys for the Foursquare API. You must fill these in, otherwise the app will not
+ * work!!
+ */
+
+class Secret {
+    static final String CLIENT_ID = "YOUR_FOURSQUARE_ID_HERE";
+    static final String CLIENT_SECRET = "YOUR_FOURSQUARE_SECRET_HERE";
+}
